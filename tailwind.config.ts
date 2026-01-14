f88521272e4b2342d2ec04f446b82557e9371bbf
Lokrogamer <lokrogamer.29@gmail.com>
2026-01-14 17:23:25 +0100

    Added Fonts to the membercounter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,6 +78,9 @@ export default {
           ring: "hsl(var(--sidebar-ring))",
         },
       },
+      fontFamily: {
+        archivo: ["'Archivo Black'", "sans-serif"],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",

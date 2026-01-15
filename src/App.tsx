180537f1dc2358e4f13a8188df3316f838292bdb
Lokrogamer <lokrogamer.29@gmail.com>
2026-01-15 19:15:02 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ const App = () => (
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<ReleasingSoon />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE 
             <Route path="/auth" element={<Auth />} />
             <Route path="/rules" element={<Rules />} />
             <Route path="/servers" element={<Servers />} />
@@ -40,7 +41,7 @@ const App = () => (
             <Route path="/partners" element={<Partners />} />
             <Route path="/server/:serverId" element={<ServerLanding />} />
             
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
